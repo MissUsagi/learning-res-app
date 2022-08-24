43f8bfd6daa2bf4aab502605c5478c69746e4ed0
MissUsagi <syljanko@gmail.com>
2022-08-24 18:49:13 +0200

    init

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+      <button :type="type" :class="mode">
+         <slot></slot>
+      </button>
+</template>
+
+<script>
+export default {
+   props: ['type', 'mode']
+}
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  margin: 5px;
+  background: white;
+  color: #ce5c00;
+  font-weight: bold;
+  border: 1px solid #ce5c00;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+   color: white;
+   background-color: #ce5c00;
+}
+
+.flat {
+   background-color: transparent;
+   color:  #ce5c00;
+   border: none;
+}
+
+.flat:hover,
+.flat:active{
+   background-color: #6e1130;
+}
+</style>
